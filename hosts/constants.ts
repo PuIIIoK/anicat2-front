43ee82173678a35e4859aa296427e53038e65d46
Пушилка <puiiiokiq@gmail.com>
2025-12-13 17:04:47 +0300

    Update SERVER_URL2 to point to new server

--- hosts/constants.ts
+++ hosts/constants.ts
@@ -7,7 +7,7 @@ export const DEBUG = true;
 
 // Видео серверы
 // PROD:
-export const SERVER_URL2 = 'https://server2.yumeko.ru'; // Сервер для загрузки видео
+export const SERVER_URL2 = 'https://server.yumeko.ru'; // Сервер для загрузки видео
 export const SERVER_URL3 = 'https://server3.yumeko.ru'; // Сервер для зарубежных пользователей
 // DEV:
 //export const SERVER_URL2 = 'http://localhost:8080'; // Сервер для загрузки видео
